3f5180d7d9170b39dcca29f52a581c095c5b3142
Daniel Toplak <homer.simpson123@gmx.de>
2025-04-28 17:52:58 +0200

    feat: basic drizzle setup

---
+++ packages/backend/drizzle/0000_striped_dreadnoughts.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "account" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "account_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"email" varchar(255) NOT NULL,
+	CONSTRAINT "account_email_unique" UNIQUE("email")
+);
