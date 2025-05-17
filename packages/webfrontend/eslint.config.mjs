42150eb92b751178ac050b6a3b57635ba783ecff
Daniel Toplak <homer.simpson123@gmx.de>
2025-05-18 00:39:40 +0200

    feat: prepare webfrontend

---
+++ packages/webfrontend/eslint.config.mjs
@@ -0,0 +1,34 @@
+import pluginReact from 'eslint-plugin-react';
+import reactPlugin from 'eslint-plugin-react-hooks';
+import { defineConfig } from 'eslint/config';
+import sharedConfig from '../../shared/eslint.config.mjs';
+
+export default defineConfig([
+  ...sharedConfig,
+
+  {
+    ignores: ['./coverage/**/*', './dist/**/*', './eslint.config.mjs', './prettier.config.mjs', 'vite.config.mjs'],
+  },
+
+  // typescript-eslint rules
+  {},
+
+  // plugin-react and plugin-react-hooks
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      react: pluginReact,
+      'react-hooks': reactPlugin,
+    },
+    rules: {
+      'react/self-closing-comp': ['error', { component: true, html: true }],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+]);
